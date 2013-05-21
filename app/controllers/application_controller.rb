7e89abfce29e53810dde3005ff79d6fd9259c9d4
destinydesigner <dd.destinydesigner@gmail.com>
2013-05-22 01:45:25 +0800

    customize devise views
    1, copy views from /Library/Ruby/Gems/1.8/gems/devise-2.2.4/app/views
    to web_site/app/views
    2, modify sessions/new.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+#    layout :devise
+    protect_from_forgery
+
+#    protected
+#
+#    def log
+#        if devise_controller?
+#            "devise"
+#        else
+#            "application"
+#        end
+#    end
 end
