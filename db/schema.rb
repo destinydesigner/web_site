ab7a7f69f8e6dac5758956141269e054de0c0d8a
Fang Guojian <dd.destinydesigner@gmail.com>
2014-12-25 14:51:45 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -35,8 +36,6 @@ ActiveRecord::Schema.define(:version => 20130522142537) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.text     "annotation"
-    t.string   "section"
-    t.integer  "level"
     t.integer  "section_id"
   end
 
